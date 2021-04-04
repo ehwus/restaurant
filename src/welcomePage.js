07e7c3e454e6a4a256143f88e4c5de9d328aee35
Alex Withington-Smith <alex@awithsmith.com>
2021-04-04 09:46:14 +0100

    added nice burger image

--- src/welcomePage.js
+++ src/welcomePage.js
@@ -1,12 +1,22 @@
 import { DomController } from './domController';
-
-const subheading = 'Welcome to the Roboto Bistro!';
-const body = document.createElement('p');
-body.innerText =
-  'We are a dog friendly, definitely real restaurant located in the heart of New Townsville! Come for the food, stay for the duration of your visit!';
+import Burger from './assets/burger.jpg';
 
 export class WelcomePage {
   static render() {
-    DomController.render(subheading, body);
+    const subheading = 'Welcome to the Roboto Bistro!';
+    DomController.render(subheading, WelcomePage._getBody());
+  }
+
+  static _getBody() {
+    const body = document.createElement('div');
+    const burgerImage = new Image();
+    burgerImage.src = Burger;
+    burgerImage.className = 'imageInBox';
+    const paragraph = document.createElement('p');
+    paragraph.innerText = `We are a dog friendly, definitely real restaurant located in the heart of New Townsville!
+      Come for the food, stay for the duration of your visit!`;
+    body.appendChild(burgerImage);
+    body.appendChild(paragraph);
+    return body;
   }
 }
